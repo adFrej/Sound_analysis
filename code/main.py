c237580e0f01c675ce5a1bdfd7b076256faa55cb
adFrej <adfrej1@gmail.com>
2022-03-31 14:38:39 +0200

    added dash

--- code/main.py
+++ code/main.py
@@ -1,6 +1,11 @@
 import numpy as np
 from scipy.io import wavfile
 import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+import webbrowser
 
 def read_file(file_path):
     sampling_rate, samples = wavfile.read(file_path)
@@ -8,7 +13,7 @@ def read_file(file_path):
 
 def draw_audio(samples):
     fig = px.line(samples)
-    fig.show()
+    return fig
 
 def volume(samples):
     return np.sqrt(np.sum(samples*samples)/len(samples))
@@ -22,9 +27,36 @@ def zcr(sampling_rate, samples):
 def sr(sampling_rate, samples):
     return volume(samples) < 0.02 and zcr(sampling_rate, samples) < 50
 
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Dźwięk - projekt 1',
+            style={'color': 'blue'}),
+    dcc.Graph(
+        id='time_graph',
+        figure=draw_audio(read_file('../wyewoluowac.wav')[1])),
+])
+
+# @app.callback(
+#     Output('', ''),
+#     Input('', ''),
+# )
+# def update_output():
+#
+
+port = 8050
+
+def open_browser():
+    webbrowser.open_new("http://localhost:{}".format(port))
+
+app.title = 'dzwiek'
+
 if __name__ == '__main__':
-    rate, samples = read_file('../wyewoluowac.wav')
-    print(volume(samples))
-    print(zcr(rate, samples))
-    print(sr(rate, samples))
-    draw_audio(samples)
+    # rate, samples = read_file('../wyewoluowac.wav')
+    # print(volume(samples))
+    # print(zcr(rate, samples))
+    # print(sr(rate, samples))
+    # draw_audio(samples)
+
+    open_browser()
+    app.run_server(debug=True)
