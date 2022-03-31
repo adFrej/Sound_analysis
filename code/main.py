89c2c318138de79a5a9a9f1de374dc56576f28f1
adFrej <adfrej1@gmail.com>
2022-03-31 15:51:28 +0200

    file upload

--- code/main.py
+++ code/main.py
@@ -1,3 +1,5 @@
+import base64
+import io
 import numpy as np
 from scipy.io import wavfile
 import plotly.express as px
@@ -32,17 +34,51 @@ app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.H1(children='Dźwięk - projekt 1',
             style={'color': 'blue'}),
+
+dcc.Upload(
+        id='upload-file',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '90%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin-left': 'auto',
+            'margin-right': 'auto',
+            'margin-top': '10px',
+            'margin-bottom': '10px',
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    ),
+
     dcc.Graph(
-        id='time_graph',
-        figure=draw_audio(read_file('../wyewoluowac.wav')[1])),
+        id='time-graph',),
 ])
 
-# @app.callback(
-#     Output('', ''),
-#     Input('', ''),
-# )
-# def update_output():
-#
+time_graph = {}
+
+@app.callback(
+    Output('time-graph', 'figure'),
+    Input('upload-file', 'contents'),
+    State('upload-file', 'filename'),
+    State('upload-file', 'last_modified'),
+)
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    global time_graph
+    if list_of_contents is not None:
+        content_type, content_string = list_of_contents.split(',')
+        file = base64.b64decode(content_string)
+        file = io.BytesIO(file)
+        rate, samples = read_file(file)
+        time_graph = draw_audio(samples)
+    return time_graph
 
 port = 8050
 
